7543e6127c947ef8bdd3ab303b6728153c4eb712
fcr3 <fcreyes@berkeley.edu>
2019-10-19 19:15:22 -0700

    Added new database constructor that actually builds off of refs

--- database_constructor.py
+++ database_constructor.py
@@ -6,65 +6,78 @@ import os
 import argparse
 
 def createArgs():
-    parser = argparse.ArgumentParser(description='Must enter username and token/password')
+    parser = argparse.ArgumentParser(description='Must enter username, token/password, and the path to the base csv')
     parser.add_argument('-u', '--user', type=str)
     parser.add_argument('-p', '--password', type=str)
+    parser.add_argument('-o', '--old_ref_path', type=str)
     return parser.parse_args()
 
-def main(user, password, type_of_req):
-    auth = user + ':' + password
-    url = 'https://api.github.com/repos/movidius/ncappzoo/traffic/' + type_of_req
-
+def grabNewDataAndStoreCSV(user, token, kind):
+    auth = user + ':' + token
+    url = 'https://api.github.com/repos/movidius/ncappzoo/traffic/' + kind
     today_dt = dt.datetime.today()
     today = '_'.join(str(today_dt).split(' '))
     today = '-'.join(str(today.split('.')[0]).split(':'))
-    folder_name = 'ncappzoo_' + type_of_req + '_' + today
-    file_name = folder_name + '/' + 'ncappzoo_' + type_of_req + '_' + today
-    os.mkdir(folder_name)
+    folder_name = 'ncappzoo_' + kind + '_' + today
+    file_name = folder_name + '/' + 'ncappzoo_' + kind + '_' + today
 
-    # Curling to get around Github Auth
-    test = subprocess.Popen(["curl", "-u", auth, url, file_name], stdout=subprocess.PIPE)
+    os.mkdir(folder_name)
+    test = subprocess.Popen(["curl", "-u", auth, url, ">", file_name], stdout=subprocess.PIPE)
     out, err = test.communicate()
-    if err:
-        sys.exit(1)
     decoded_string = out.decode("utf-8").replace("'", '""').replace('\n', '')
     data = json.loads(decoded_string)
-
-    # Converting to DataFrame
-    timestamp_data = data[type_of_req]
+    
+    counts = data['count']
+    uniques = data['uniques']
+    timestamp_data = data[kind]
     df = pd.DataFrame(timestamp_data)
+    
     timestamps = [x.split('T')[0] for x in df['timestamp'].values]
     df['day'] = [x.split('-')[2] for x in timestamps]
     df['mo'] = [x.split('-')[1] for x in timestamps]
     df['yr'] = [x.split('-')[0] for x in timestamps]
+    df['collection_timestamp'] = [today_dt for x in range(len(timestamps))]
     
-    # Writing JSON
     json_string = json.dumps(data, indent=4, sort_keys=True)
     f = open(file_name + '.json', 'a')
     f.write(json_string)
     f.close()
+    csv_string = df.to_csv(file_name + '.csv', index=False)
+    return file_name + '.csv'
+
+def construct_new_ref(old_file_path, new_file_path, kind):
+    old_master_df = pd.read_csv(old_file_path)
+    new_df = pd.read_csv(new_file_path)
+    yrs = [x for x in new_df['yr'].values]
+    mos = [x for x in new_df['mo'].values]
+    dys = [x for x in new_df['day'].values]
+    new_df['yr_mo_day'] = [dt.datetime(yrs[i], mos[i], dys[i]) for i in range(len(yrs))]
     
-    # Writing CSV
-    df.to_csv(file_name + '.csv', index=False)
+    # Construction of new ref
+    new_master = old_master_df.append(new_df).groupby('timestamp').max().reset_index()
+    yrs = [x for x in new_master['yr'].values]
+    mos = [x for x in new_master['mo'].values]
+    dys = [x for x in new_master['day'].values]
+    new_master['yr_mo_day'] = [dt.datetime(yrs[i], mos[i], dys[i]) for i in range(len(yrs))]
     
-    # Creating New Master Files
-    old_file = 'master_tables/master_' + type_of_req + '_table.csv'
-    old_master = pd.read_csv(old_file)
-    new_master = old_master.append(df).groupby('timestamp', sort=True).agg('first')
-    master_csv_name = 'master_tables/master_' + type_of_req + '_table'
-    new_master.reset_index().to_csv(master_csv_name + '.csv', index=False)
-    
-    counts = data['count']
-    uniques = data['uniques']
-    old_master_sum = pd.read_csv('master_tables/master_' + type_of_req + '_summary.csv')
-    new_master_sum_dict = [{'collection_timestamp': str(today_dt), 'counts': counts, 'uniques': uniques}]
-    new_master_sum = pd.DataFrame(new_master_sum_dict)
-    new_master_sum = old_master_sum.append(new_master_sum).groupby('collection_timestamp').agg('first')
-    master_csv_summary_name = 'master_tables/master_' + type_of_req + '_summary'
-    new_master_sum.reset_index().to_csv(master_csv_summary_name + '.csv', index=False)
+    today_dt = dt.datetime.today()
+    today = '_'.join(str(today_dt).split(' '))
+    today = '-'.join(str(today.split('.')[0]).split(':'))
+    file_name = 'temp/updated_' + kind + '_' + today
+    csv_string = new_master.to_csv(file_name + '.csv', index=False)
+    return file_name + '.csv'
     
 if __name__ == "__main__":
     args = createArgs()
-    main(args.user, args.password, 'views')
-    main(args.user, args.password, 'clones')
+    
+    new_file = grabNewDataAndStoreCSV(args.user, args.password,'views')
+    old_ref = args.old_ref_path
+    new_ref_views = construct_new_ref(old_ref, new_file, 'views')
+    
+    new_file = grabNewDataAndStoreCSV(args.user, args.password,'clones')
+    old_ref = args.old_ref_path
+    new_ref_clones = construct_new_ref(old_ref, new_file, 'clones')
+    
+    print('New Clones Reference File:', new_ref_clones)
+    print('New Views Reference File:', new_ref_views)
     
